27a794caa889749a37550ea21fa6c8df18d8fba9
Babitababy <kumaribabita2402@gmail.com>
2021-04-20 09:03:51 +0530

    Chaaracter Animation

--- character_animation.py
+++ character_animation.py
@@ -1,35 +1,68 @@
 import pygame
+
 pygame.init()
 
-win =pygame.display.set_mode((500,500))
-pygame.display.set_caption("Character Animation")
+win = pygame.display.set_mode((500, 500))
+pygame.display.set_caption("First Game")
 
-x = 250
-y = 250
-radius = 15
-val = 10
+walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'),
+             pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png'),
+             pygame.image.load('R7.png'), pygame.image.load('R8.png'), pygame.image.load('R9.png')]
+walkLeft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'), pygame.image.load('L3.png'),
+            pygame.image.load('L4.png'), pygame.image.load('L5.png'), pygame.image.load('L6.png'),
+            pygame.image.load('L7.png'), pygame.image.load('L8.png'), pygame.image.load('L9.png')]
+bg = pygame.image.load('bg.jpg')
+char = pygame.image.load('standing.png')
 
-run= True
+x = 50
+y = 50
+width = 40
+height = 60
+vel = 5
+
+isJump = False
+jumpCount = 10
+
+left = False
+right = False
+walkCount = 0
+
+
+def redrawGameWindow():
+    global walkCount
+
+    win.blit(bg, (0, 0))  # This will draw our background image at (0,0)
+    pygame.display.update()
+
+
+run = True
+
 while run:
-    win.fill((0,0,0))
-    pygame.draw.circle(win, (255, 255, 255), (int(x), int(y)), radius)
+    pygame.time.delay(100)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
+    keys = pygame.key.get_pressed()
 
-    # MOVEMENT OF CHARACTER
-    userInput = pygame.key.get_pressed()
-    if userInput[pygame.K_LEFT]:
-        x -= val
-    if userInput[pygame.K_RIGHT]:
-        x += val
-    if userInput[pygame.K_UP]:
-        y -= val
+    if keys[pygame.K_LEFT] and x > vel:
+        x -= vel
 
-    if userInput[pygame.K_DOWN]:
-        y += val
+    if keys[pygame.K_RIGHT] and x < 500 - vel - width:
+        x += vel
 
-    pygame.time.delay(10)
-    pygame.display.update()
+    if not (isJump):
+        if keys[pygame.K_SPACE]:
+            isJump = True
+    else:
+        if jumpCount >= -10:
+            y -= (jumpCount * abs(jumpCount)) * 0.5
+            jumpCount -= 1
+        else:
+            jumpCount = 10
+            isJump = False
+
+    redrawGameWindow()
+
+pygame.quit()
